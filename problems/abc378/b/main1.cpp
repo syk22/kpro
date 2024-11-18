37d32efb10c40dd0406b7055c8b95bea28b28af5
SayakaNakajima <andsayaka@gmail.com>
2024-11-18 09:47:53 +0900

    378 B

--- problems/abc378/b/main1.cpp
+++ problems/abc378/b/main1.cpp
@@ -2,23 +2,9 @@
 using namespace std;
 
 #define REP(i, n) for (int i = 0; i < (n); ++i)
-#define ALL(x) (x).begin(),(x).end()
-
-// #define REP(i, a, n) for (int i = (a); i < (n); ++i)
-#define FORE(i, a) for (auto &i : a)
-#define RALL(x) (x).rbegin(),(x).rend()
-#define RREP(i, a, n) for (int i = (a); i >= (n); --i)
-#define REPS(i, a, n, b) for (int i = (a); i < (n); i += (b))
-
-// 10^9以上10^18以下はllを使用
-using ll = long long;
-// long long は (2^63 - 1) が最高なので、それ以上の時はunsignedをつける必要がある　10^19まで
-using ull = unsigned long long;
 
 int N, Q, t, d;
-int ans = 0, flag = 0;
-string S;
-string res = "Yes";
+int ans = 0;
 
 int main() {
   cin >> N;
@@ -28,19 +14,21 @@ int main() {
 
   cin >> Q;
 
-  // vector<pair<int, int>> q(Q);
-  // REP (i, N) cin >> gomi[i].first >> gomi[i].second;
-
-
   REP (i, Q) {
     cin >> t >> d, --t;
+    int base = (d / gomi[t].first) * gomi[t].first;
+    d -= base;
+    ans = base + gomi[t].second;
+    if (d > gomi[t].second) ans += gomi[t].first;
+    cout << ans << endl;
   }
-
-  cout << ans << endl;
-  cout << res << endl;
 
   return 0;
 }
 
 // 2024/11/10
 // 4:43
+// 2024/11/14
+// 14:52
+// 2024/11/18
+// 5分ぐらい
